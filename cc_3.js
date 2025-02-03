2321cf047252fd541030a70338d3c5ae51ad505f
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-02 22:28:40 -0500

    Task 1 - Product Price Management.

---
+++ cc_3.js
@@ -0,0 +1,12 @@
+// Task 1
+// Declaring an array with prices 
+let prices = [10, 20, 30, 40, 50];
+
+// Adding a new price to the array
+prices.push(60);
+
+// Removing the first price from the array
+prices.shift();
+
+// Logging the updated price list 
+console.log("Updated prices", prices);
